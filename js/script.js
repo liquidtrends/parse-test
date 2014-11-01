8a2fa2b61b7b6fec04acb6efc5bad2a9dfd7f0dd
Ryan Janvier <ryan@liquidtrends.com>
2014-11-01 00:52:34 -0400

    Add slider to index

--- js/script.js
+++ js/script.js
@@ -35,3 +35,15 @@ $(function() {
 
 });
 
+$("[data-slider]")
+    .each(function () {
+      var input = $(this)
+      $("<span>")
+        .addClass("output")
+        .insertAfter($(this));
+    })
+    .bind("slider:ready slider:changed", function (event, data) {
+      $(this)
+        .nextAll(".output:first")
+          .html(data.value.toFixed(3));
+    });
